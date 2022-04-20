34a2c742912acd459cd1d13ebb12a11408f036ca
muhamedmahu <91724573+muhamedmahu@users.noreply.github.com>
2022-04-19 22:01:01 -1100

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import './Header.css'
 export const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+      <div className="container header__container"> 
+      <h1>BMW</h1>
+      <h2>BMW M3 E36</h2>
+      <h3>5000$</h3>
+      </div>
+   </header>
   )
 }
