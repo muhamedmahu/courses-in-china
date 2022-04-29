3c787e54cb7f612e96fece0870e73fb9ca57ea5e
muhamedmahu <91724573+muhamedmahu@users.noreply.github.com>
2022-04-29 22:31:24 +0600

    commit not

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -4,7 +4,7 @@ import {FaGithub} from 'react-icons/fa'
 import {BsTwitter} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
+    <div className='header__socials header__container'  >
 <a href="https://linkedin.com" target="blank"><BsLinkedin/></a>
 <a href="https://github.com" target="blank"><FaGithub/></a>
 <a href="https://twitter.com" target="blank"><BsTwitter/></a>
