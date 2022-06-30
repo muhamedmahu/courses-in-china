05b78e6305d48c27d0e3516f7643cc4d4cf023c4
muhamedmahu <91724573+muhamedmahu@users.noreply.github.com>
2022-06-30 20:29:47 +0600

    Experienced

--- src/components/expirience/Expirience.jsx
+++ src/components/expirience/Expirience.jsx
@@ -1,8 +1,98 @@
 import React from 'react'
 import './Expirience.css'
-function Expirience() {
+import {BsPatchCheckFill} from 'react-icons/bs'
+const Expirience = () => {
   return (
-    <section id='experience'>Expirience</section>
+    <section id='expirience'>
+<h5>What Skills I Have</h5>
+<h2>My Experienced</h2>
+
+<div className="cntainer experienced__container" >
+  <div className="experienced__frontend">
+    <h3>Frontent developer</h3>
+    <div className="experinced__content">
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>HTML</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>CSS</h4>
+<small className='text-light'>intermediate</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>JavaScript</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>Tailwild</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>Bootstrap</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>React</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+    </div>
+  </div>
+</div>
+
+<div className="experienced__backend">
+<h3>Backedn developer</h3>
+    <div className="experinced__content">
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>HTML</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>CSS</h4>
+<small className='text-light'>intermediate</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>JavaScript</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>Tailwild</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>Bootstrap</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+
+      <article className='experience__detail'>
+<BsPatchCheckFill />
+<h4>React</h4>
+<small className='text-light'>Experienced</small>
+      </article>
+    </div>
+  </div>
+
+
+    </section>
   )
 }
 
