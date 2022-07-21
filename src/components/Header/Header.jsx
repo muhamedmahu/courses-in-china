7b458c107c392726ef78a4aab4fa6769f4fecbeb
muhamedmahu <91724573+muhamedmahu@users.noreply.github.com>
2022-07-21 14:06:42 +0600

    me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import './Header.css'
-import CTA from './CTA'
+
 import ME from "../../assets/me.png";
-import HeaderSocials from './HeaderSocials';
+
 export const Header = () => {
   return (
    <header>
       <div className="container header__container"> 
 <h3>впервые в городе каракол</h3>
 <h1>Экспресс Бизнес курсы с Китаем</h1>
-    <CTA />
-    <HeaderSocials />
+   
     <div className='me'>
       <img src={ME} alt="me" />
     </div>
